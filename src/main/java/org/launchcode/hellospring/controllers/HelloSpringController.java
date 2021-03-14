3cda9c2a6a535255668074496a56c19030d1d384
Sandy Hadley <sandymariehadley@gmail.com>
2021-03-14 17:21:02 -0500

    Select menu now visible

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -11,11 +11,9 @@ import java.util.List;
  * Created by Chris Bay
  */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloSpringController {
 
-//        //Handles requests at path /hello
+        //Handles requests at path /hello
 //    @GetMapping("hello") //<--"hello" specifies which path our handler should live at.
 //    public String hello() {
 //        return "Hello, Spring!";
@@ -23,23 +21,25 @@ public class HelloSpringController {
 
 //    Handles requests at path /goodbye
 //    Lives at /hello/goodbye
-    @GetMapping("goodbye") //<--"goodbye" specifies which path our handler should live at.
-    public String goodbye() {
-        return "Goodbye, Spring!";
-
-    }
-//     Live at hello/hello
+//    @GetMapping("goodbye") //<--"goodbye" specifies which path our handler should live at.
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//
+//    }
+//    Lives at hello/hello
 //    Handles request of the form /hello?name=LaunchCode
-    @GetMapping("hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String helloWithQueryParamOld(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//
+//    }
 
     //Path Parameter, or Variable, handles requests of the form /hello/LaunchCode
     //@PathVariable takes an argument that, if matching a portion of the URL, will deliver this data into the handler.
     //name is a placeholder, indicating where in the URL segment to look for the @PathVariable
-//    @GetMapping("{name}")
+//    @GetMapping("hello/{name}")
+//    @ResponseBody
 //    //User can be redirected by removing the @ResponseBody annotation from the controller and returning "redirect:/DESIREDPATH"
 //    public String helloWithPathParameter(@PathVariable String name) {
 //        return "Hello, " + name + "!";
@@ -49,18 +49,20 @@ public class HelloSpringController {
     // Remember, @RequestMapping can annotate a method that responds to both GET and POST.
     // Live at hello/hello
     //Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
-        return "Hello, " + name + "! Which " + language + "would you like to be greeted in?";
+    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
-    //Lives at /hello/form
+//    //Lives at /hello/form
     @GetMapping("form")
+    @ResponseBody
     public String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action='hello'>" + //Submit a request to /hello
+                "<form action='hello' method='post'>" + //Submit a request to /hello
                 "<input type='text' name='name'>" +
-                "<select name='language'>" +
+                "<select value='language'>" +
                     "<option value='French'>French</option>" +
                     "<option value='German'>German</option>" +
                     "<option value='Spanish'>Spanish</option>" +
